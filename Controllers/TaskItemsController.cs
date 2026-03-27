36da25a012e7050de7fb398170093253bfa4639a
LucasYamazaki <lu.yamazaki@gmail.com>
2026-03-27 10:33:37 -0300

    Primeiro commit - Task Manager

---
+++ Controllers/TaskItemsController.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Data;
+using TaskManager.Models;
+
+namespace TaskManager.Controllers
+{
+    public class TaskItemsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TaskItemsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Listar tarefas 
+        public async Task<IActionResult> Index(string filter)
+        {
+            var tasks = _context.Tasks.AsQueryable();
+
+            if (filter == "pendentes")
+            {
+                tasks = tasks.Where(t => !t.IsCompleted);
+            }
+            else if (filter == "concluidas")
+            {
+                tasks = tasks.Where(t => t.IsCompleted);
+            }
+
+            return View(await tasks.ToListAsync());
+        }
+
+        // Criar tarefa (get)
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Criar tarefa (post)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(TaskItem task)
+        {
+            if (ModelState.IsValid)
+            {
+                task.CreatedAt = DateTime.Now;
+
+                ModelState.Remove("Description");
+
+                if (string.IsNullOrWhiteSpace(task.Description))
+                {
+                    task.Description = "-Sem descrição-";
+                }
+                _context.Add(task);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(task);
+        }
+
+        // Editar tarefa (get)
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null) return NotFound();
+
+            return View(task);
+        }
+
+        // Editar tarefa (post)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, TaskItem task)
+        {
+            if (id != task.Id) return NotFound();
+
+            var existingTask = await _context.Tasks.FindAsync(id);
+
+            if (existingTask == null)
+                return NotFound();
+
+            existingTask.Title = task.Title;
+
+            if (string.IsNullOrWhiteSpace(task.Description))
+            {
+                existingTask.Description = "-Sem descrição-";
+            }
+            else
+            {
+                existingTask.Description = task.Description;
+            }
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // Deletar tarefa
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null) return NotFound();
+
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Complete(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+
+            if (task == null)
+                return NotFound();
+
+            task.IsCompleted = true;
+            task.CompletedAt = DateTime.Now;
+
+            _context.Update(task);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+        // Reabrir tarefas fechadas
+        public async Task<IActionResult> Reopen(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+
+            if (task == null)
+                return NotFound();
+
+            task.IsCompleted = false;
+            task.CompletedAt = null;
+
+            _context.Update(task);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Models/TaskItem.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O título é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O título deve ter no máximo 100 caracteres.")]
+        public string Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? CompletedAt { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
